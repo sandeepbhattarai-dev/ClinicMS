b23480ae3fc75f756e8d7d3e34242e16b2d53e94
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-09-14 13:46:02 +0530

    changed minor, first setup of domain is completed

--- ClinicMS.Domain/Entities/ApplicationUser.cs
+++ ClinicMS.Domain/Entities/ApplicationUser.cs
@@ -5,5 +5,10 @@ namespace ClinicMS.Domain.Entities
 {
   public class ApplicationUser : IdentityUser
   {
+    //FullName, IsActive, CreatedOn, and nullable LastLogin
+    public string FullName { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedOn { get; set; }
+    public DateTime? LastLogIn { get; set; }
   }
 }
