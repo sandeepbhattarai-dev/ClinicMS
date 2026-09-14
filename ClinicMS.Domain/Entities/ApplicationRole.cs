847f235cfc9b44611fe3d5b4cc51accd731f070d
sandeepdev-hub <bhattarai.dev01@gmail.com>
2026-09-14 10:37:42 +0530

    initial domain setup done, ready to merge

---
+++ ClinicMS.Domain/Entities/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicMS.Domain.Entities
+{
+  public class ApplicationRole : IdentityRole<Guid>
+  {
+  }
+}
